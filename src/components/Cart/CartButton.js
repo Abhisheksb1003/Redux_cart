5aae1002bf19a0203cc8657f90218cd49e235bcf
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-12-22 19:25:42 +0530

    Add to Cart Fixes

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,20 @@
 import classes from './CartButton.module.css';
+import { useDispatch , useSelector } from 'react-redux';
+import { uiActions } from '../../Store/UISlice';
 
 const CartButton = (props) => {
+
+  const cartQuantity =  useSelector(state => state.cart.totalQuantity)
+
+  const dispatch = useDispatch();
+
+  const tooglecartHandler=()=>{
+    dispatch(uiActions.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={tooglecartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
